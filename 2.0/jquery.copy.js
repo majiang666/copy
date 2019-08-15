92f1655e8bd5712a82dada27ce0384e9dfbfb427
cssking <laji198888@163.com>
2019-08-15 17:23:57 +0800

    优化复制成功提示为用户自定义

--- 2.0/jquery.copy.js
+++ 2.0/jquery.copy.js
@@ -1,21 +1,12 @@
 ;(function($){
 	var defaults = {
-		imgUrl:"",//分享图标
-		text:"复制成功",//分享提示文案
 		content:"",//自定义复制链接地址
-		tipTime:2000,//分享提示消失时间
-		tip:true//是否显示提示
+		callback:null//成功回调
 	};
 	$.extend({
 		copy:function(option){
 			var options = $.extend({},defaults,option);
 			var content = options.content == "" ? "请配置复制内容" : options.content;
-			var IMG = options.imgUrl == "" ? "" : '<img style="width: 22px;" src="'+options.imgUrl+'">';
-			var TIP = options.tip;
-			var tipsHtml = '<div id="share-tips" style="position: fixed;top: 50%;left:50%;background: rgba(0,0,0,.5);border-radius: 4px;margin: 0 auto;color: #fff;z-index: 9999;padding: 5px 10px;font-size: 14px;text-align: center;transform: translate(-50%,-50%);">'+
-		        IMG+
-		        '<p>'+options.text+'</p>'+
-		    '</div>';
 		    var u = navigator.userAgent; 
 			var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端 
 			var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端 
@@ -42,13 +33,8 @@
 				window.getSelection().addRange(range);
 				document.execCommand('copy'); 
 			}
-			if(TIP){
-				$("body").append(tipsHtml);
-				setTimeout(function(){
-					$("#share-tips").remove();
-				},options.tipTime);
-			}
 			$("#selector").remove();
+			if(options.callback) options.callback();
 		}
 	});
 })(jQuery);
